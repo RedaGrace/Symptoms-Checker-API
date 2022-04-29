c08fa83acda7ed3f75a5c161ad6b9da7244d5d7a
Reda Grace <41922411+RedaGrace@users.noreply.github.com>
2022-04-29 17:29:32 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+from flask import *
+import pandas as pd
+import json, time
+import joblib
+import re
+
+
+
+filename = 'xgb_model.joblib'
+model = joblib.load(filename)
+outlier = joblib.load('outlier.joblib')
+precautions = pd.read_csv('precautions.csv')
+
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET'])
+def home_page():
+    data_set = {'Page': 'Home', 'Message': "Let's get started and send me your symptoms", 'Timestamp': time.time()}
+    json_dump = json.dumps(data_set)
+    return json_dump
+
+@app.route('/predict/', methods=['GET'])
+def request_page():
+    symptoms = request.args.get('symptoms') # /predict/?symptoms=symptoms
+    symptoms= re.findall('\d', symptoms)
+    symptoms= list(map(int,symptoms))
+
+    out = outlier.predict([symptoms])[0]
+    if out == 1:
+        data_set = {'message' : 'You can see a real doctor for better result since your symptoms are away of my knowledge'}
+    else:
+        disease = model.predict([symptoms])[0]
+
+        data_set = {'prediction': disease, 'Timestamp': time.time(),
+               "precaution_1": precautions[precautions.Disease == disease].iloc[0][2],
+               "precaution_2": precautions[precautions.Disease == disease].iloc[0][3],
+               "precaution_3": precautions[precautions.Disease == disease].iloc[0][4],
+               "precaution_4": precautions[precautions.Disease == disease].iloc[0][5]}
+    json_dump = json.dumps(data_set)
+    return json_dump
+
+if __name__=='__main__':
+    app.run(port=7777)
